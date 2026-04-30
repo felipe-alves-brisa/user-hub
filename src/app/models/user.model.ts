3f98effc29485baf457b0a55cd660d5daa7cc5d5
felipe alves <alves.felipe@grupobrisanet.com.br>
2026-04-30 16:21:25 -0300

    refactor: add address, company, user, and post interfaces to the user model.

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,29 @@
+export interface Address {
+  street: string;
+  suite?: string;
+  city: string;
+  zipcode: string;
+}
+
+export interface Company {
+  name: string;
+  catchPhrase?: string;
+  bs?: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone?: string;
+  website?: string;
+  address?: Address;
+  company: Company;
+}
+
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
