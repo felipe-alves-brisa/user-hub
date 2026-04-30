933395e4e3656758aac9a2183c068536f5b00a89
felipe alves <alves.felipe@grupobrisanet.com.br>
2026-04-30 16:43:23 -0300

    refactor: ajust format code

--- src/app/pages/user-list/user-modal/user-modal.component.ts
+++ src/app/pages/user-list/user-modal/user-modal.component.ts
@@ -1,73 +1,82 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { IonModalService } from "@brisanet/ion";
-import { User } from "../../../services/user.service";
+import { User } from "../../../models/user.model";
 
 @Component({
-    selector: "app-user-modal",
-    templateUrl: `./user-modal.component.html`,
-    styleUrls: ["./user-modal.component.css"]
+  selector: "app-user-modal",
+  templateUrl: `./user-modal.component.html`,
+  styleUrls: ["./user-modal.component.css"],
 })
 export class UserModalComponent implements OnInit {
-    @Input() user: User | undefined;
+  @Input() user: User | undefined;
 
-    form!: FormGroup;
+  form!: FormGroup;
 
-    constructor(
-        private fb: FormBuilder,
-        private modalService: IonModalService
-    ) {}
+  constructor(
+    private fb: FormBuilder,
+    private modalService: IonModalService,
+  ) {}
 
-    ngOnInit() {
-        const u = this.user;
+  ngOnInit() {
+    const u = this.user;
 
-        this.form = this.fb.group({
-            name: [u && u.name ? u.name : "", Validators.required],
-            email: [
-                u && u.email ? u.email : "",
-                [Validators.required, Validators.email],
-            ],
-            companyName: [
-                u && u.company && u.company.name
-                    ? u.company.name
-                    : "",
-                Validators.required,
-            ],
-            phone: [u && u.phone ? u.phone : ""],
-            website: [u && u.website ? u.website : ""],
-        });
+    this.form = this.fb.group({
+      name: [u ? u.name : "", Validators.required],
+      email: [u ? u.email : "", [Validators.required, Validators.email]],
+      companyName: [
+        u && u.company && u.company.name ? u.company.name : "",
+        Validators.required,
+      ],
+      phone: [u ? u.phone : ""],
+      website: [u ? u.website : ""],
+    });
+  }
+
+  get name() {
+    return this.form.get("name");
+  }
+
+  get email() {
+    return this.form.get("email");
+  }
+
+  get companyName() {
+    return this.form.get("companyName");
+  }
+
+  get phone() {
+    return this.form.get("phone");
+  }
+
+  get website() {
+    return this.form.get("website");
+  }
+
+  save() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
     }
 
-    get name() {return this.form.get("name"); }
-    get email() {return this.form.get("email"); }
-    get companyName() {return this.form.get("companyName"); }
-    get phone() {return this.form.get("phone"); }
-    get website() {return this.form.get("website"); }
+    const formValues = this.form.value;
 
-    save() {
-        if (this.form.invalid) {
-            this.form.markAllAsTouched();
-            return;
-        }
+    const editedUser: User = {
+      id: this.user ? this.user.id : 0,
+      name: formValues.name,
+      email: formValues.email,
+      phone: formValues.phone,
+      website: formValues.website,
+      address: this.user ? this.user.address : undefined,
+      company: {
+        name: formValues.companyName,
+      },
+    };
 
-        const formValues = this.form.value;
+    this.modalService.emitValueAndCloseModal({ editedUser });
+  }
 
-        const editedUser: User = {
-            id: this.user ? this.user.id : 0,
-            name: formValues.name,
-            email: formValues.email,
-            phone: formValues.phone,
-            website: formValues.website,
-            address: this.user ? this.user.address : undefined,
-            company: {
-                name: formValues.companyName,
-            },
-        };
-
-        this.modalService.emitValueAndCloseModal({ editedUser });
-    }
-
-    cancel() {
-        this.modalService.closeModal();
-    }
+  cancel() {
+    this.modalService.closeModal();
+  }
 }
